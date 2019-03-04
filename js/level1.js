6e20185115761368f99023ffa32e22ea619ac0b5
Madeline Rombes <madelinerombes@gmail.com>
2019-03-04 16:00:09 -0500

    3 random divs function, not yet implemented into flip

--- js/level1.js
+++ js/level1.js
@@ -1,9 +1,18 @@
 let blocks = document.getElementsByClassName('block')
-// let blocksArray = [...blocks]
-
-
+let blockFlip = function() {
 for (let i=0; i<blocks.length; i++) {
     blocks[i].addEventListener('click', function() {
         event.target.style.backgroundColor = 'blue'
     })
+}}
+
+let blockArray = [...blocks]
+
+let randomBlocks = function() {
+    for (let i=0; i<3; i++) {
+    console.log(blockArray[Math.floor(Math.random() * blockArray.length)])
+    }
 }
+
+console.log(randomBlocks())
+
