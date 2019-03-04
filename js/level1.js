fc2d0011d03a4719735f4930d9c84cc8215a019a
Madeline Rombes <madelinerombes@gmail.com>
2019-03-04 17:22:22 -0500

    miscellaneous fixes

--- js/level1.js
+++ js/level1.js
@@ -8,17 +8,11 @@ for (let i=0; i<blocks.length; i++) {
     })
 }}
 
-//MORE UBIQUITOUS FLIP BLOCK FUNCTION
-
-// let randomBlockFlip = function(block) {
-//     block.
-// }
-
-
 
 //GET FOUR RANDOM BLOCKS TO FLIP
 
 let blockArray = [...blocks]
+
 
 let randomBlock = function() {
     return blockArray[Math.floor(Math.random() * blockArray.length)]
@@ -31,6 +25,12 @@ randomBlocksArr.push(randomBlock())
 randomBlocksArr.push(randomBlock())
 randomBlocksArr.push(randomBlock())
 
-for (let i=0; i<randomBlocksArr.length; i++) {
-    randomBlocksArr[i].style.backgroundColor = 'blue'
-}
+//BUG: THIS IS ALLOWING FOR REPEATS. NEED TO FIX.
+
+let startTime = setTimeout(function(){
+    for (let i=0; i<randomBlocksArr.length; i++) {
+        randomBlocksArr[i].style.backgroundColor = 'blue'
+    }}, 500)
+
+
+
