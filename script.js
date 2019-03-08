0c6f34da5d706e64ff08694773e4a54995e8cf7a
Madeline Rombes <madelinerombes@gmail.com>
2019-03-07 22:08:41 -0500

    changed colors and other fixes

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const level1Grid = document.querySelector('.tile-container')
+const grid = document.querySelector('.tile-container')
 const score = document.querySelector('#score')
-const footer = document.querySelector('.footer')
 const landingPageBtn = document.querySelector('#landing-page-btn')
 const instructions = document.querySelector('h2')
 
 
 let landingPage = function() {
     score.style.display = 'none'
-    footer.style.display = 'none'
-    level1Grid.style.display = 'none'
+    grid.style.display = 'none'
 }
 
 landingPage()
@@ -18,25 +16,27 @@ const tileArray = [...tiles]
 let points = 0
 let winningArray = []
 let userFlipArray = []
+let winningArrayEval = []
+let userFlipArrayEval = []
 
 let playLevelOne = function() {
 
-    level1Grid.style.display = 'flex'
+    grid.style.display = 'flex'
     score.style.display = 'block'
-    footer.style.display = 'block'
     landingPageBtn.style.display = 'none'
     instructions.style.display = 'none'
 
 let flipTile = function() {
     for (let i=0; i<tileArray.length; i++) {
-        tileArray[i].style.backgroundColor = '#f4bc42'}
+        tileArray[i].style.backgroundColor = '#A95097'}
 }
 
 let resetTiles = function() {
     flipTile()
     winningArray = []
     userFlipArray = []
-    flipFourRandomTiles()
+    userFlipArrayEval = []
+    flipFiveRandomTiles()
     userTileFlip()
 }
 
@@ -45,45 +45,54 @@ let playGame = function() {
     flipTile()
     winningArray = []
     userFlipArray = []
-    flipFourRandomTiles()
+    flipFiveRandomTiles()
     userTileFlip()
 }
 
 let randomTilesTimeout = function() {
     setTimeout(() => {
         for (let i=0; i<winningArray.length; i++) {
-            winningArray[i].style.backgroundColor = 'white'
+            winningArray[i].style.backgroundColor = '#3C537C'
         }}, 1000)
     setTimeout(() => {
         for (let i=0; i<winningArray.length; i++) {
-            winningArray[i].style.backgroundColor = '#f4bc42'
+            winningArray[i].style.backgroundColor = '#A95097'
         }}, 1750)
     }
 
 let randomize = function() {
-for (let i=0; i<4; i++) {
+for (let i=0; i<5; i++) {
     let randomTile = tileArray[Math.floor(Math.random() * tileArray.length)]
     winningArray.push(randomTile)
     let index = tileArray.indexOf(randomTile)
     tileArray.splice(index, 1)}
 }
 
-let flipFourRandomTiles = function() {
+
+let flipFiveRandomTiles = function() {
     randomTilesTimeout()
     randomize()
     tileArray.push(winningArray[0])
     tileArray.push(winningArray[1])
     tileArray.push(winningArray[2])
     tileArray.push(winningArray[3])
+    tileArray.push(winningArray[4])
 }
+
+let newArr = []
 
 let compareArrays = function(array1, array2) {
     for (let i=0; i<array2.length; i++) {
         if (array1.includes(array2[i])) {
-            return true
+            newArr.push(true)
         } else {
-            return false
-}}}
+            newArr.push(false)
+    }} if ((newArr[0] === true) && (newArr[1] === true) && (newArr[2] === true) && (newArr[3] === true) && (newArr[4] === true)) {
+        return true
+    } else {
+        return false
+    }
+}
 
 let gainOnePoint = function() {
     document.querySelector('.score').innerText = points += 1;  
@@ -94,35 +103,67 @@ let gainOnePoint = function() {
 
 let lose = function() {
     setTimeout(() => {
-        let playAgain = window.confirm('rats, you lose. play again?')
+        let playAgain = window.confirm('Rats, you lost. Want to try again?')
         if (playAgain === true) {
         playGame()}
     }, 250)
 }
 
 let winAlert = function() {
-    if (document.querySelector('.score').innerText == 3) { let levelTwoQuery = window.confirm('YOU WON LEVEL 1! Move to Level 2?')
-    if (levelTwoQuery === true) {
-        playLevelTwo()
+    if (document.querySelector('.score').innerText == 3) { let playAgainQuery = window.confirm('YOU WON THE GAME! Want to play again?')
+    if (playAgainQuery === true) {
+        playGame()
     }
 }}
 
+
+
 let userTileFlip = function() {
     for (let i=0; i<tileArray.length; i++) {
-        tileArray[i].addEventListener('click', function() {event.target.style.backgroundColor = 'white'
+        tileArray[i].addEventListener('click', function() {event.target.style.backgroundColor = '#3C537C'
     })
     tileArray[i].onclick = function() {
         userFlipArray.push(tileArray[i])
-        if (userFlipArray.length === 4) {
-            if (compareArrays(userFlipArray, winningArray) === true) {
+
+
+
+
+        if (userFlipArray.length === 5) {
+            let userId1 = userFlipArray[0].getAttribute('id')
+            let userId2 = userFlipArray[1].getAttribute('id')
+            let userId3 = userFlipArray[2].getAttribute('id')
+            let userId4 = userFlipArray[3].getAttribute('id')
+            let userId5 = userFlipArray[4].getAttribute('id')
+            userFlipArrayEval.push(userId1)
+            userFlipArrayEval.push(userId2)
+            userFlipArrayEval.push(userId3)
+            userFlipArrayEval.push(userId4)
+            userFlipArrayEval.push(userId5)
+            console.log(userFlipArrayEval)
+            console.log(winningArrayEval)
+            setTimeout(() => { if (compareArrays(winningArrayEval, 
+                userFlipArrayEval) === true) {
                 gainOnePoint()
-            } else { lose() }
-            winAlert()
+                winAlert()
+            } else if (compareArrays(winningArrayEval, 
+                userFlipArrayEval) === false) { lose(); resetTiles()
+            }}, 500)
 }}}}
 
-flipFourRandomTiles()
+flipFiveRandomTiles()
 userTileFlip()
 
+
+let winId1 = winningArray[0].getAttribute('id')
+let winId2 = winningArray[1].getAttribute('id')
+let winId3 = winningArray[2].getAttribute('id')
+let winId4 = winningArray[3].getAttribute('id')
+let winId5 = winningArray[4].getAttribute('id')
+
+winningArrayEval.push(winId1)
+winningArrayEval.push(winId2)
+winningArrayEval.push(winId3)
+winningArrayEval.push(winId4)
+winningArrayEval.push(winId5)
+
 }
-
-
